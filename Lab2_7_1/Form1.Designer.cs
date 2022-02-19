4e4d567bb4b233ff3f426e615a350e9e4195d1d2
Danil <33226455+SDGame@users.noreply.github.com>
2022-02-19 18:01:18 +0500

    Lab7_1

---
+++ Lab2_7_1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace Lab7_2_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "Mas[0] = ",
+            "Mas[1] = ",
+            "Mas[2] = ",
+            "Mas[3] = ",
+            "Mas[4] = ",
+            "Mas[5] = ",
+            "Mas[6] = ",
+            "Mas[7] = ",
+            "Mas[8] = ",
+            "Mas[9] = ",
+            "Mas[01] = ",
+            "Mas[11] = ",
+            "Mas[12] = ",
+            "Mas[13] = ",
+            "Mas[14] = ",
+            "Mas[15] = ",
+            "Mas[16] = ",
+            "Mas[17] = ",
+            "Mas[18] = ",
+            "Mas[19] =  "});
+            this.listBox1.Location = new System.Drawing.Point(13, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(200, 382);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Items.AddRange(new object[] {
+            "Mas[0] = ",
+            "Mas[1] = ",
+            "Mas[2] = ",
+            "Mas[3] = ",
+            "Mas[4] = ",
+            "Mas[5] = ",
+            "Mas[6] = ",
+            "Mas[7] = ",
+            "Mas[8] = ",
+            "Mas[9] = ",
+            "Mas[01] = ",
+            "Mas[11] = ",
+            "Mas[12] = ",
+            "Mas[13] = ",
+            "Mas[14] = ",
+            "Mas[15] = ",
+            "Mas[16] = ",
+            "Mas[17] = ",
+            "Mas[18] = ",
+            "Mas[19] =  "});
+            this.listBox2.Location = new System.Drawing.Point(219, 39);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(200, 382);
+            this.listBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Исходный массив:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(226, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Полученный массив:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(13, 427);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Заполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(229, 427);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Замена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 478);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab2_7_1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_2_1
+{
+    public partial class Form1 : Form
+    {
+        int[] Mas = new int[20];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Очищаем элемент управления
+            listBox1.Items.Clear();
+            // Инициализируем класс случайных чисел
+            Random rand = new Random();
+            // Генерируем и выводим 15 элементов
+            Mass render = new Mass();
+            for (int i = 0; i < 20; i++)
+            {
+                Mas[i] = rand.Next(-50, 50);
+                if (Mas[i] > render.max)           //Поиск максимального значения и его индекса
+                {
+                    render.max = Mas[i];
+                    render.i = i;
+                }
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                if (i == render.i)
+                {
+                    listBox1.Items.Add("Mas[" + i.ToString() + "] = " + Mas[i].ToString() + " <==Макс");
+                }
+                else
+                {
+                    listBox1.Items.Add("Mas[" + i.ToString() + "] = " + Mas[i].ToString());
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Очищаем элемент управления
+            listBox2.Items.Clear();
+            // Обрабатываем все элементы
+            Mass mass = new Mass();
+            int Zero = Mas[0];
+            for (int i = 0; i < 20; i++)        //Поиск максимального значения и его индекса
+            {
+                if (Mas[i] > mass.max)
+                {
+                    mass.max = Mas[i];
+                    mass.i = i;
+                }
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Mas[mass.i] = Zero;
+                Mas[0] = mass.max;
+                if(i == 0)
+                {
+                    listBox2.Items.Add("Mas[" + Convert.ToString(i) + "] = " + Mas[i].ToString() + " <==Макс");
+                }
+                else if(i == mass.i)
+                {
+                    listBox2.Items.Add("Mas[" + Convert.ToString(i) + "] = " + Mas[i].ToString() + " <==[0]-индекс");
+                }
+                else
+                {
+                    listBox2.Items.Add("Mas[" + Convert.ToString(i) + "] = " + Mas[i].ToString());
+                }
+            }
+        }
+        struct Mass     //Структура для запоминания наибольшего числа и индекса этого числа
+        {
+            public int max;
+            public int i;
+        }
+    }
+}
