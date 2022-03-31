5869c8bed4703e9f53abdb21bb0e1ae4d1866b0b
Danil <33226455+SDGame@users.noreply.github.com>
2022-03-31 16:55:16 +0300

    Add files via upload

--- Lab2_5_14/Form1.cs
+++ Lab2_5_14/Form1.cs
@@ -12,48 +12,48 @@ namespace Lab14_5_2
 {
     public partial class Form1 : Form
     {
-        Point lastPoint;
-        Point Point;
+        Point lastPoint;        //Переменная координат точек для информации о нахождении мышки
+        Point Point;            //Переменная координат точек для размешения объектов
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        private void Form1_MouseMove(object sender, MouseEventArgs e)   //Вывод координат мыши в правый верхний угл для справшки
         {
-            lastPoint = new Point(e.X, e.Y);
+            lastPoint = new Point(e.X, e.Y);    
             this.textBox1.Text = Convert.ToString(lastPoint.X) + "; " + Convert.ToString(lastPoint.Y);
         }
 
-        private void Start_Click(object sender, EventArgs e)
+        private void Start_Click(object sender, EventArgs e)    //При нажатии на кнопку обработки
         {
-            Button b = new Button();
-            TextBox tb = new TextBox();
-            Label lb = new Label();
+            Button b = new Button();        //Инициализация эклезмпляра класса для динамического создания кнопки
+            TextBox tb = new TextBox();     //Инициализация эклезмпляра класса для динамического создания поля ввода
+            Label lb = new Label();         //Инициализация эклезмпляра класса для динамического создания label
             try
             {
-                Point.X = Convert.ToInt32(x_point.Text);
-                Point.Y = Convert.ToInt32(y_point.Text);
+                Point.X = Convert.ToInt32(x_point.Text);    //Получение значения X из поля ввода
+                Point.Y = Convert.ToInt32(y_point.Text);    //Получения значения Y из поля ввода
             }
             catch
             {
-                MessageBox.Show("Неправильно введены координаты!", "Ошибка", MessageBoxButtons.OK);
+                MessageBox.Show("Неправильно введены координаты!", "Ошибка", MessageBoxButtons.OK); //Вывод сообщения ошибки при неправильных координатах
                 return;
             }
-            string type = type_text.Text;
-            if (type.Contains("K") || type.Contains("k") || type.Contains("К") || type.Contains("к"))
+            string type = type_text.Text;       //Получение типа генерируемого объекта
+            if (type.Contains("K") || type.Contains("k") || type.Contains("К") || type.Contains("к"))   //При К делаем кнопку
             {
                 b.Parent = this;
                 b.Location = new Point(Point.X, Point.Y);
                 b.Text = "button";
             }
-            else if(type.Contains("П") || type.Contains("п"))
+            else if(type.Contains("П") || type.Contains("п"))       //При П делаем поле ввода
             {
                 tb.Parent = this;
                 tb.Location = new Point(Point.X, Point.Y);
                 tb.Text = "TextBox";
             }
-            else if (type.Contains("М") || type.Contains("м") || type.Contains("M") || type.Contains("m"))
+            else if (type.Contains("М") || type.Contains("м") || type.Contains("M") || type.Contains("m"))      //При М делаем метку
             {
                 lb.Parent = this;
                 lb.Location = new Point(Point.X, Point.Y);
@@ -61,11 +61,11 @@ namespace Lab14_5_2
             }
             else
             {
-                MessageBox.Show("Нет ни одной подходящей буквы!", "Ошибка", MessageBoxButtons.OK);
+                MessageBox.Show("Нет ни одной подходящей буквы!", "Ошибка", MessageBoxButtons.OK);      //Выводим ошибку если введено что-то другое
             }
         }
 
-        private void type_text_KeyDown(object sender, KeyEventArgs e)
+        private void type_text_KeyDown(object sender, KeyEventArgs e)        //При нажатии Enter в поле ввода запускается метод Start_Click
         {
             if (e.KeyCode == Keys.Enter)
             {
@@ -81,7 +81,7 @@ namespace Lab14_5_2
                 e.SuppressKeyPress = true;
                 Start_Click(sender, e);
             }
-        }
+        }       //При нажатии Enter в поле ввода запускается метод Start_Click
 
         private void y_point_KeyDown(object sender, KeyEventArgs e)
         {
@@ -90,6 +90,6 @@ namespace Lab14_5_2
                 e.SuppressKeyPress = true;
                 Start_Click(sender, e);
             }
-        }
+        }       //При нажатии Enter в поле ввода запускается метод Start_Click
     }
 }
