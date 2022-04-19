a7a00dd2a6bda0ba55e76846139d5f8c4363fd9a
Danil <33226455+SDGame@users.noreply.github.com>
2022-04-19 20:24:57 +0300

    Lab2_10

---
+++ Lab2_10_1/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Lab2_10_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа №10";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Lab2_10_1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_10_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen MyPen0 = new Pen(Brushes.Black, 2);
+            MyPen0.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+            Pen MyPen1 = new Pen(Brushes.Blue, 3);
+            MyPen1.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDotDot;
+            Pen MyPen2 = new Pen(Brushes.Gray, 4);
+            MyPen2.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+            g.DrawLine(MyPen0, 10, 20, 870, 20);
+            g.DrawEllipse(MyPen1, 40, 40, 100, 100);
+            g.DrawArc(MyPen2, 150, 40, 100, 100, 0, -180);
+            g.FillRectangle(Brushes.Black, 280, 40, 100, 100);
+            g.FillEllipse(Brushes.BlueViolet, 400, 40, 100, 100);
+        }
+    }
+}
