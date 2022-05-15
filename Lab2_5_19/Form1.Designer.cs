99ea8b8801e8e2cac4445dabb9cef225ba8db9ab
Danil <33226455+SDGame@users.noreply.github.com>
2022-05-16 01:55:41 +0300

    Lab2_5_19

---
+++ Lab2_5_19/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Lab2_5_19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(28, 29);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "TextBox";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(28, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(56, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Button";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(28, 77);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(51, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Label";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(119, 98);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(138, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(838, 523);
+            this.panel2.TabIndex = 4;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 547);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа 5";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Lab2_5_19/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_5_19
+{
+    public partial class Form1 : Form
+    {
+        Point lastPoint;        //Класс координат для создания элементов
+        Point lastPoint2;       //Класс координат для внесения изменений
+        private bool flag = false;      //Флаг первого элемента
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Start()        //Общий метод динамического создания элементов
+        {
+            if (radioButton1.Checked)       //Если выбран textBox
+            {   
+                TextBox a = new TextBox();      //Создание элемента TextBox для динамической генерации
+                a.Text = "TextBox";             //Пишем тект, который будет внутри
+                a.Parent = panel2;              //Назначаем родительский элемент
+                lastPoint = Point(102);         //Вызываем метод для получения координат
+                a.Location = new Point(lastPoint.X, lastPoint.Y);       //Создаём элемент по координатам, которые получили
+            }
+            else if (radioButton2.Checked)      //Если выбран Button
+            {
+                Button b = new Button();
+                b.Text = "Button";
+                b.Parent = panel2;
+                lastPoint = Point(102);
+                b.Location = new Point(lastPoint.X, lastPoint.Y);
+            }
+            else if (radioButton3.Checked)      //Если выбран Label
+            {
+                Label lb = new Label();
+                lb.Text = "Label";
+                lb.Parent = panel2;
+                lastPoint = Point(102);
+                lb.Location = new Point(lastPoint.X, lastPoint.Y);
+            }
+        }
+        private void panel2_MouseDown(object sender, MouseEventArgs e)      //При нажатии на Panel
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                Start();
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)       //При нажатии на Form
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Start();
+            }
+        }
+        private Point Point(int Tab)        //Функция переноса строки с сдвига по вертикали
+        {
+            if (lastPoint2.X >= 700)        //Ушли за форму?
+            {
+                flag = true;                //Поднять флаг
+                lastPoint2.X = 0;           //Обнулить вертикаль
+                lastPoint2.Y += 22;         //Перенести строку
+            }
+            else if (!flag)                 //Опущен флаг? Первый символ
+            {
+                lastPoint2.X = 0;           //Обнулить вертикаль
+                flag = true;                //Поднять флаг 
+            }
+            else                            //Если не первый элемент, то 
+            {
+                lastPoint2.X += Tab;        //Назначаем новую позицию для горизонтали
+            }
+            return lastPoint2;              //Возвращаем все координаты
+        }
+    }
+}
